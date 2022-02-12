b3d6fb3b234aeb99b823866b1f4bd276c8da8273
Carlos Caicedo <calicheccmusica@gmail.com>
2022-02-12 13:43:11 -0800

    initial commit

---
+++ src/main/java/com/carloscaicedo/hellohuman/HellohumanController.java
@@ -0,0 +1,19 @@
+package com.carloscaicedo.hellohuman;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellohumanController {
+
+	@RequestMapping("/")
+    public String index(@RequestParam(value="name", required=false) String name) {
+		if (name == null) {
+			return "Hello Earthling!";
+		} else {
+			return "Hello " + name;		
+		}
+    }		
+}
+
---
+++ src/test/java/com/carloscaicedo/hellohuman/HelloHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carloscaicedo.hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
